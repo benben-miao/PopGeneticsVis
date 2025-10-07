fc137acf6cf13994fc861cd500c4362eeb4503ae
benben-miao <benben.miao@outlook.com>
2025-10-07 17:03:23 +0800

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,13 @@
+library(argparse)
+
+library(methylKit)
+library(GenomicFeatures)
+library(rtracklayer)
+library(GenomicRanges)
+
+library(dplyr)
+library(tidyr)
+
+library(ggplot)
+library(ggpubr)
+library(CMplot)
